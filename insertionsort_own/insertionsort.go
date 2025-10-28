e8ef3e230e84aa8f5b5ba6d28ece8b4170986c92
Robin Erhardt <67264436+RobinErhardt@users.noreply.github.com>
2025-10-28 17:01:05 +0100

    More exercises added

---
+++ insertionsort_own/insertionsort.go
@@ -0,0 +1,16 @@
+package insertionsort
+
+// MoveLeft bewegt das Element an Stelle i so lange nach links,
+// bis es an der richtigen Stelle im bereits sortierten Teil der Liste ist.
+func MoveLeft(arr []int, i int) {
+	for j := i; j > 0 && arr[j] < arr[j-1]; j-- {
+		arr[j], arr[j-1] = arr[j-1], arr[j]
+	}
+}
+
+// InsertionSort sortiert die übergebene Liste mittels des Insertion-Sort-Algorithmus.
+func InsertionSort(arr []int) {
+	for i := 1; i < len(arr); i++ {
+		MoveLeft(arr, i)
+	}
+}
